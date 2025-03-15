49bbb12376a5d9bb909a21434650089809242ad6
yamjoram <99007890+yamjoram@users.noreply.github.com>
2025-03-16 00:33:56 +0530

    day_184.sql

--- day_184.sql
+++ day_184.sql
@@ -14,3 +14,20 @@ FROM
     UNION ALL SELECT 3 UNION ALL SELECT 4 UNION ALL SELECT 5 UNION ALL SELECT 6 
     UNION ALL SELECT 7 UNION ALL SELECT 8 UNION ALL SELECT 9 UNION ALL SELECT 10 
     UNION ALL SELECT 11) AS months;
+
+#### alternative ###
+
+
+select current_date(), 
+       date_sub(current_date(), interval n month) as mnth,
+       last_day(date_sub(current_date(), interval n month)) as lastDayOfMonth,
+       dayofweek(last_day(date_sub(current_date(), interval n month))) as day,
+       date_sub(last_day(date_sub(current_date(), interval n month)),
+                interval (dayofweek(last_day(date_sub(current_date(), interval n month))) + 1) % 7 day
+                ) as lst_friday
+  from (
+select 0 as n union all select 1 union all select 2 union all select 3 union all select 4
+union all select 5  UNION ALL SELECT 6 UNION ALL SELECT 7 UNION ALL SELECT 8
+UNION ALL SELECT 9 UNION ALL SELECT 10 UNION ALL SELECT 11) as months;
+
+
