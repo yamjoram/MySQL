5807ae83b7916ff601719113cd373015c5119eba
yamjoram <99007890+yamjoram@users.noreply.github.com>
2025-04-04 15:26:12 +0530

    day_218.sql(creating table)

---
+++ day_218.sql
@@ -0,0 +1,8 @@
+
+Questions : Write an SQL query to create a table named Employees with the following columns:
+
+create table employees(
+employee_id int primary key auto_increment,
+first_name varchar(50) not null,
+last_name varchar(50) not null,
+hire_date date);
